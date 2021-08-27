867b41f810463833fefa64e993ed7db16b9a9f43
Fatih Atalay <64832858+atalayf@users.noreply.github.com>
2021-08-27 10:49:12 -0400

    Update ebay_file.processor.py
    Cleanup and edit comments.

--- ebay_file.processor.py
+++ ebay_file.processor.py
@@ -12,52 +12,37 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-#
-
-
 
 import pandas as pd
 import glob
 import os
 from datetime import date
 
-
-
-# get the latest downloaded ebay sales report
-
+# Get the latest downloaded ebay sales report (We read the whole report since they are less than 200K for the amount 
+# of orders we process.)
 list_of_files = glob.glob('C:\\Users\\atala\\Downloads\\eBay-OrdersReport*.csv')
 latest_ebay_oders = max(list_of_files, key=os.path.getctime)
 
-
-# read sales report into pandas dataframe
+# Read sales report into pandas dataframe. 
 sales = pd.read_csv(latest_ebay_oders ,skiprows=range(1))
 
-
-
-#drop the first empty line
+# Drop the first empty line
 sales=sales.drop(sales.index[0])
-#drop last 3 lines which are not orders
+# Drop last 3 lines which are not orders
 sales=sales.drop(sales.index[[-1,-2,-3]])
-
-
-
 
 # 1 - Prefixing Quantity in the Custom Label Column
 # Change column quantity from float>int>str.
 sales['Quantity'] = sales['Quantity'].astype(int).astype(str)
-# Prefixing Qty in the CustomLabel Column
-
 
 for i in range(1, sales.shape[0] + 1):
     if  pd.isnull(sales.loc[i, 'Custom Label'])    :
         pass
     elif sales.loc[i,'Quantity'] != '1':
-
         sales.loc[i,'Custom Label'] = str(sales.loc[i,'Quantity']) + 'x ' + str(sales.loc[i,'Custom Label'])
 
-
-
-# 2 - Combine Mutiple Item Purchases (within same order)
+# 2 - Combine Mutiple Item Purchases (within same order) These orders have the same 'Sales Recod Number' and they
+# in the consecutive lines
 i=0
 while i < sales.shape[0]-1:
     if (sales.iloc[i,0]) == (sales.iloc[i+1,0]):  #find multiple item orders
@@ -65,7 +50,6 @@ while i < sales.shape[0]-1:
         rec = sales.iloc[i,0]
         cart = ""
         j = i
-
         while (rec == sales.iloc[j, 0]):  # concat custom labels.
             if pd.isnull(sales.loc[j+1, 'Custom Label']):
                 pass
@@ -81,21 +65,9 @@ while i < sales.shape[0]-1:
 
     i += 1
 
-
 # 3 - Combine Mutiple orders from same buyer.
-
 # add a column to identify multiple orders from the same buyer that ships to the same address.
 sales['mult_order'] = 0
-
-
-i=1
-for j in range(i + 1, sales.shape[0] + 1):
-    if (sales.loc[i, 'Buyer Username'] == sales.loc[j, 'Buyer Username']) and \
-            (sales.loc[i, 'Ship To Address 1'] == sales.loc[j, 'Ship To Address 1']):
-        sales.loc[i, 'mult_order'] = i
-        sales.loc[j, 'mult_order'] = i
-
-
 
 for i in range(2, sales.shape[0]+1):
     for j in range(i+1, sales.shape[0]+1):
@@ -118,9 +90,7 @@ for i in range(1, sales.shape[0]+1):
                 sales.loc[j, 'Custom Label']=""
         sales.loc[i, 'Custom Label'] = cart
 
-
 #drop the multiple order identifier column
-
 sales.drop('mult_order',1, inplace=True)
 
 today = date.today()
